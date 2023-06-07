a82daaed1d66aa0655b5128ff8f3dfbdeaed1720
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2023-06-07 17:30:28 +0530

    [add] : Navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 export default function Home() {
-  return <div className="text-center ">hi</div>;
+  return (
+    <div className="relative px-6">
+      <Navbar />
+      <Hero />
+    </div>
+  );
 }
