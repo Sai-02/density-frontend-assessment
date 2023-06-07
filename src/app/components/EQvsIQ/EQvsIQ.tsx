59820f09499b1d79e1e6f65ad4da371fccc7ae3a
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2023-06-07 17:44:45 +0530

    [add] : EQvsIQ section added

---
+++ src/app/components/EQvsIQ/EQvsIQ.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const EQvsIQ = () => {
+  return (
+    <section className="mt-20 grid grid-cols-3 h-[20rem] px-10 gap-8">
+      <div className="text-2xl text-bold text-center">EQ beats IQ</div>
+      <div className="text-xl">
+        People with high emotional intelligence (EQ) live more fulfilled lives.
+        They tend to behappier and have healtheir relationships.
+      </div>
+      <div className="text-xl">
+        They are more successful in their pursuits and make forinspiring
+        leaders. According to science, they earns $29 a year
+      </div>
+    </section>
+  );
+};
+
+export default EQvsIQ;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import EQvsIQ from "./components/EQvsIQ/EQvsIQ";
 
 export default function Home() {
   return (
     <div className="relative px-6">
       <Navbar />
       <Hero />
+      <EQvsIQ />
     </div>
   );
 }
